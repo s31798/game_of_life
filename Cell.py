3a3512c95d52dca6653837b7d051c4125261435f
Potapus <aliemirbjk@hotmail.com>
2025-04-22 12:04:40 +0200

    fixed the boundaries and made counting faster by reducing loops

--- Cell.py
+++ Cell.py
@@ -1,4 +1,8 @@
 class Cell:
+    NEIGHBOR_OFFSETS = [(-1, -1), (-1, 0), (-1, 1),
+                        (0, -1), (0, 1),
+                        (1, -1), (1, 0), (1, 1)]
+
     def __init__(self, x, y, is_alive=False):
         self.x = x
         self.y = y
@@ -6,16 +10,11 @@ class Cell:
         self.will_be_alive = is_alive
 
     def count_alive_neighbours(self, cell_list):
-        alive_count = 0
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == 0 and j == 0:
-                    continue #skip self
-                neighbour_x, neighbour_y = self.x + i, self.y + j
-                if 0 <= neighbour_x <= len(cell_list) and 0 <= neighbour_y <= len(cell_list[0]):
-                    if cell_list[neighbour_x][neighbour_y].is_alive:
-                        alive_count += 1
-        return alive_count
+        return sum(
+            0 <= self.x + dx < len(cell_list)
+            and 0 <= self.y + dy < len(cell_list[0])
+            and cell_list[self.x + dx][self.y + dy].is_alive
+            for dx, dy in Cell.NEIGHBOR_OFFSETS)
 
     def calculate_cell_state(self, cell_list):
         alive_count = self.count_alive_neighbours(cell_list)
@@ -23,7 +22,7 @@ class Cell:
         if self.is_alive:
             self.will_be_alive = alive_count in [2, 3]  # 2 or 3 live neighbors otherwise dies
         else:
-            self.will_be_alive = alive_count == 3 # if dead, revives with 3 neighbors
+            self.will_be_alive = alive_count == 3  # if dead, revives with 3 neighbors
 
     def update(self):
         self.is_alive = self.will_be_alive
